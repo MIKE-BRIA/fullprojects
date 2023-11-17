801be1fb2ba7c5b19d081d666e7f496d78cb72b5
Brian <brianmichaeladero@gmail.com>
2023-11-17 18:27:30 +0300

    http requests

---
+++ HTTPRequestfetch()/public/scripts/comment.js
@@ -0,0 +1,7 @@
+//working with http request from the client side
+
+const loadCommentsBtnElement = document.getElementById("load-comments-btn");
+
+function fetchCommentsForPost() {}
+
+loadCommentsBtnElement.addEventListener("click", fetchCommentsForPost);
---
+++ HTTPRequestfetch()/routes/blog.js
@@ -0,0 +1,158 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const db = require("../data/database");
+
+const ObjectId = mongodb.ObjectId;
+
+const router = express.Router();
+
+//route to homepage
+
+router.get("/", function (req, res) {
+  res.redirect("/posts");
+});
+
+router.get("/posts", async function (req, res) {
+  const posts = await db
+    .getDb()
+    .collection("posts")
+    .find({}, { title: 1, summary: 1, "author.name": 1 })
+    .toArray();
+  res.render("posts-list", { posts: posts });
+});
+
+//route to page of creating new post and finding authors name
+
+router.get("/new-post", async function (req, res) {
+  const authors = await db.getDb().collection("authors").find().toArray();
+  res.render("create-post", { authors: authors });
+});
+
+//route to submitting entered data on creating new post to the database
+
+router.post("/posts", async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db
+    .getDb()
+    .collection("authors")
+    .findOne({ _id: authorId });
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  console.log(result);
+  res.redirect("/posts");
+});
+
+//route to viewing the entire posts
+
+router.get("/posts/:id", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { summary: 0 });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  post.humanReadableDate = post.date.toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  post.date = post.date.toISOString();
+
+  res.render("post-detail", { post: post, comments: null });
+});
+
+//route for editing an existing post the get request to render the page with data from the database
+
+router.get("/posts/:id/edit", async function (req, res) {
+  const postId = req.params.id;
+  const post = await db
+    .getDb()
+    .collection("posts")
+    .findOne({ _id: new ObjectId(postId) }, { title: 1, summary: 1, body: 1 });
+
+  if (!post) {
+    return res.status(404).render("404");
+  }
+
+  res.render("update-post", { post: post });
+});
+
+//route to submitting edited data from the edit page to the database
+
+router.post("/posts/:id/edit", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const result = await db
+    .getDb()
+    .collection("posts")
+    .updateOne(
+      { _id: postId },
+      {
+        $set: {
+          title: req.body.title,
+          summary: req.body.summary,
+          body: req.body.content,
+          // date: new Date()
+        },
+      }
+    );
+
+  res.redirect("/posts");
+});
+
+//route to deleting post from the database
+
+router.post("/posts/:id/delete", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const result = await db
+    .getDb()
+    .collection("posts")
+    .deleteOne({ _id: postId });
+  res.redirect("/posts");
+});
+
+//route to to the full post together with the comment section
+
+router.get("/posts/:id/comments", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const post = await db.getDb().collection("posts").findOne({ _id: postId });
+  const comments = await db
+    .getDb()
+    .collection("comments")
+    .find({ postId: postId })
+    .toArray();
+
+  return res.render("post-detail", { post: post, comments: comments });
+});
+
+// route to submitting the contents of the comment section to the database
+
+router.post("/posts/:id/comments", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const newComment = {
+    postId: postId,
+    title: req.body.title,
+    text: req.body.text,
+  };
+  await db.getDb().collection("comments").insertOne(newComment);
+  res.redirect("/posts/" + req.params.id);
+});
+
+module.exports = router;
