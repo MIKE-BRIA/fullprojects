6a1667dbd47f4caca1e507dc96b32fb295d4167b
Brian <brianmichaeladero@gmail.com>
2023-11-19 06:27:30 +0300

    all routes for the project

--- user-authentication/routes/demo.js
+++ user-authentication/routes/demo.js
@@ -1,29 +1,112 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 const router = express.Router();
 
-router.get('/', function (req, res) {
-  res.render('welcome');
+//route to the home-page
+
+router.get("/", function (req, res) {
+  res.render("welcome");
 });
 
-router.get('/signup', function (req, res) {
-  res.render('signup');
+//route to the sign-up page
+
+router.get("/signup", function (req, res) {
+  res.render("signup");
 });
 
-router.get('/login', function (req, res) {
-  res.render('login');
+//route to the login page
+
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
-router.post('/signup', async function (req, res) {});
+//route that collect sign-up data to the database
 
-router.post('/login', async function (req, res) {});
+router.post("/signup", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredConfirmEmail = userData["confirm-email"]; //we use the brackets because of the dash
+  const enteredPassword = userData.password;
 
-router.get('/admin', function (req, res) {
-  res.render('admin');
+  //checking userdata
+  if (
+    !enteredEmail ||
+    !enteredConfirmEmail ||
+    !enteredPassword ||
+    enteredPassword.trim() < 6 ||
+    enteredEmail !== enteredConfirmEmail ||
+    !enteredEmail.includes("@")
+  ) {
+    console.log("Please enter the required data");
+    return res.redirect("/signup");
+  }
+
+  //checking if user exist already
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (existingUser) {
+    ConnectionClosedEvent.log("User already exists");
+    return res.redirect("/signup");
+  }
+
+  //hashing passwords
+  const hashedPassword = await bcrypt.hash(enteredPassword, 12); // 12 is the strength of the hash
+
+  const user = {
+    email: enteredEmail,
+    password: hashedPassword,
+  };
+
+  await db.getDb().collection("users").insertOne(user);
+
+  res.redirect("/login");
 });
 
-router.post('/logout', function (req, res) {});
+//route that collect login data to the database
+
+router.post("/login", async function (req, res) {
+  const userData = req.body;
+  const enteredEmail = userData.email;
+  const enteredPassword = userData.password;
+  //checking if the login user exist in our database
+  const existingUser = await db
+    .getDb()
+    .collection("users")
+    .findOne({ email: enteredEmail });
+
+  if (!existingUser) {
+    console.log("could not login");
+    return res.redirect("/login");
+  }
+  //checking if the password entered in equal to the hashed on in the database
+  const passwordsAreEqual = await bcrypt.compare(
+    enteredPassword,
+    existingUser.password
+  );
+
+  if (!passwordsAreEqual) {
+    console.log("could not login-seems like you entered the wrong password");
+    return res.redirect("/login");
+  }
+
+  console.log("User is authenticated");
+  res.redirect("/admin");
+});
+
+//route that render the admin page
+
+router.get("/admin", function (req, res) {
+  res.render("admin");
+});
+
+//route that seems to delete user data from the database
+
+router.post("/logout", function (req, res) {});
 
 module.exports = router;
