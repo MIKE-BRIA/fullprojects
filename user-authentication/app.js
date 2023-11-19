b683f314cd2e0cc83422fda759779ad6166bd68c
Brian <brianmichaeladero@gmail.com>
2023-11-19 06:25:16 +0300

    the server

--- user-authentication/app.js
+++ user-authentication/app.js
@@ -1,23 +1,33 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
+const session = require("express-session");
 
-const db = require('./data/database');
-const demoRoutes = require('./routes/demo');
+const db = require("./data/database");
+const demoRoutes = require("./routes/demo");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
+//middleware for creating sessions
+app.use(
+  session({
+    secret: "super-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use(function (error, req, res, next) {
+  res.render("500");
+});
 
 db.connectToDatabase().then(function () {
   app.listen(3000);
